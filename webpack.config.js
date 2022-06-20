9d46050fd0e69d4d58d94be1958e673b99ac5bea
ucr_lixian <593771171@qq.com>
2022-06-20 15:53:54 +0800

    webpack打包html资源

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const { resolve } = require("path")
+// 引用插件 html-webpack-plugin
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     // 入口
@@ -16,7 +18,25 @@ module.exports = {
     },
 
     // 插件
-    plugins: [],
+    plugins: [
+        // 构造方法默认会创建一个空的HTML文件，自动引入打包输出的所有资源（js/css）
+        // new HtmlWebpackPlugin()
+
+        // 通过参数可以输出有结构的HTML资源
+        new HtmlWebpackPlugin({
+            // 指定要打包的HTML，并自动引入打包输出的所有资源
+            template: './src/index.html',
+            // 设置输出文件名称，默认index.html
+            filename: 'index.html',
+            // 功能: 压缩HTML代码，生产模式自动压缩
+            minify: {
+                // 移除空格
+                collapseWhitespace: true,
+                // 移除注释
+                removeComments: true
+            }
+        })
+    ],
 
     // 模式
     mode:"development"
